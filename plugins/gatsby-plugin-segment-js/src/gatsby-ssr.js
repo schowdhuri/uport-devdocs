5ce338966a0f830cf14214eededa30946dbe4897
gbugyis <greg@bugyis.com>
2018-07-19 17:59:40 +0300

    Passing zero IP as a string

--- plugins/gatsby-plugin-segment-js/src/gatsby-ssr.js
+++ plugins/gatsby-plugin-segment-js/src/gatsby-ssr.js
@@ -16,7 +16,7 @@ exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
   const writeKey = process.env.NODE_ENV === "production" ? prodKey : devKey;
 
   // if trackPage option is falsy (undefined or false), remove analytics.page(), else keep it in by default
-  const includeTrackPage = !trackPage ? "" : "analytics.page({}, {context: { ip: 0.0.0.0 }});";
+  const includeTrackPage = !trackPage ? "" : "analytics.page({}, {context: { ip: '0.0.0.0' }});";
 
   // Segment's minified snippet (version 4.0.0)
   const snippet = `!function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t){var e=document.createElement("script");e.type="text/javascript";e.async=!0;e.src=("https:"===document.location.protocol?"https://":"http://")+"cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)};analytics.SNIPPET_VERSION="4.0.0";
