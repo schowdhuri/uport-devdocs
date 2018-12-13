c37a235d18dc826ab45d6ee9665e14a6ffd9edca
gbugyis <greg@bugyis.com>
2018-12-13 09:58:12 +0200

    Adding appBranding when initializing uPortConnect

--- src/utilities/uPortConnectSetup.js
+++ src/utilities/uPortConnectSetup.js
@@ -2,7 +2,11 @@ import { Connect } from 'uport-connect'
 let uPortConnect = {}
 
 if (typeof window !== 'undefined') {
-  uPortConnect = new Connect('MyApps')
+  uPortConnect = new Connect('MyApps', {
+    profileImage: {'/': '/ipfs/QmdKanPraYmgJzaPiNMx5r4D8VixnkezeZrMiG1mw6fbwk'},
+    bannerImage: {'/': '/ipfs/QmTFNFu1v4dev6YCDoMuSG9Zi3EubagUJ4LQxoZkMiBPSF'},
+    description: 'uPort Developer Portal'
+  })
 }
 
 export {uPortConnect}
